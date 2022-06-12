85bbec2bc38654e144213cb6d192434bb1b6379f
N2H4 <n.nawrocki@onet.pl>
2022-06-12 20:21:41 +0200

    create app

---
+++ app/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace wierzcholki_rozdzielajace
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputTextbox = new System.Windows.Forms.TextBox();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(493, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Program do znajdowania wierzchołków rozdzielających grafu";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 84);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1024, 442);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Location = new System.Drawing.Point(12, 532);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(184, 23);
+            this.calcBtn.TabIndex = 2;
+            this.calcBtn.Text = "Znajdź wierzchołki rozdzielające";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.calcBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(877, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wprowadź Graf. Nazwy wierzchołków mogą składać się z cyfr i liter. Do pola Sąsied" +
+    "zi wpisz listę połączonych wierzchołków, nazwy wierzchołków rozdziel jedną spacj" +
+    "ą.";
+            // 
+            // outputTextbox
+            // 
+            this.outputTextbox.Location = new System.Drawing.Point(12, 583);
+            this.outputTextbox.Multiline = true;
+            this.outputTextbox.Name = "outputTextbox";
+            this.outputTextbox.ReadOnly = true;
+            this.outputTextbox.Size = new System.Drawing.Size(1024, 122);
+            this.outputTextbox.TabIndex = 4;
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(961, 57);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(75, 23);
+            this.resetBtn.TabIndex = 5;
+            this.resetBtn.Text = "Resetuj";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1135, 717);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.outputTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.calcBtn);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Wierzchołki Rozdzielające";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button calcBtn;
+        private Label label2;
+        private TextBox outputTextbox;
+        private Button resetBtn;
+    }
+}
---
+++ app/algorytm.cs
@@ -0,0 +1,84 @@
+﻿namespace Algorytm
+{
+    public class Graph
+    {
+        private int V;
+
+        private List<int>[] adj;
+        int time = 0;
+        static readonly int NIL = -1;
+
+        public Graph(int v)
+        {
+            V = v;
+            adj = new List<int>[v];
+            for (int i = 0; i < v; ++i)
+                adj[i] = new List<int>();
+        }
+
+        public void addEdge(int v, int w)
+        {
+            adj[v].Add(w);
+            adj[w].Add(v); 
+        }
+
+        void APUtil(int u, bool[] visited, int[] disc,
+                    int[] low, int[] parent, bool[] ap)
+        {
+
+            int children = 0;
+
+            visited[u] = true;
+
+            disc[u] = low[u] = ++time;
+
+            foreach (int i in adj[u])
+            {
+                int v = i;
+
+                if (!visited[v])
+                {
+                    children++;
+                    parent[v] = u;
+                    APUtil(v, visited, disc, low, parent, ap);
+
+                    low[u] = Math.Min(low[u], low[v]);
+
+
+                    if (parent[u] == NIL && children > 1)
+                        ap[u] = true;
+
+                    if (parent[u] != NIL && low[v] >= disc[u])
+                        ap[u] = true;
+                }
+
+                else if (v != parent[u])
+                    low[u] = Math.Min(low[u], disc[v]);
+            }
+        }
+
+        public bool[] AP()
+        {
+            bool[] visited = new bool[V];
+            int[] disc = new int[V];
+            int[] low = new int[V];
+            int[] parent = new int[V];
+            bool[] ap = new bool[V];
+
+
+            for (int i = 0; i < V; i++)
+            {
+                parent[i] = NIL;
+                visited[i] = false;
+                ap[i] = false;
+            }
+
+            for (int i = 0; i < V; i++)
+                if (visited[i] == false)
+                    APUtil(i, visited, disc, low, parent, ap);
+
+            return ap;
+        }
+
+    }
+}
